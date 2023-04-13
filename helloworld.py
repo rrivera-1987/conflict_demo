d34326f9cf3fdf60767ed38415051837ca43d0d9
Ramon Rivera <ramonrivera550@gmail.com>
2023-04-13 11:19:03 -0400

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-def hello_world(state):
-    print(f'Hello World {state}!')
-
-
-def main():
-    hello_world('New York')
-=======
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
->>>>>>> 998322caddea0c5fa906f8f5544ba31cf55da3f1
 
 
 main()
